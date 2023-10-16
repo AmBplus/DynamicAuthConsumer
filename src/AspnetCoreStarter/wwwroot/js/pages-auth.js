0d43f4bdadc93a16ed4a61b609ce95e4b87595f6
Amir <96792239+AmBplus@users.noreply.github.com>
2023-10-16 12:47:18 +0330

    feat redirect uesr to home pages

--- src/AspnetCoreStarter/wwwroot/js/pages-auth.js
+++ src/AspnetCoreStarter/wwwroot/js/pages-auth.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
                     text: data.message
                   });
                 }
-                window.location.replace(window.location.hostname);
+                window.location = '/';
               
               });
 
