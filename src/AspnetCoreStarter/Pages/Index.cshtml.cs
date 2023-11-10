566a35dcdf41efa6a5c5afb71da2ab825db946d2
Amir <96792239+AmBplus@users.noreply.github.com>
2023-11-10 22:40:05 +0330

    feat all alert toas page message

--- src/AspnetCoreStarter/Pages/Index.cshtml.cs
+++ src/AspnetCoreStarter/Pages/Index.cshtml.cs
@@ -1,10 +1,31 @@
+using Bootstrap5ToastExample.Classes;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetCoreStarter.Pages
 {
-  public class IndexModel : PageModel
+  public class IndexModel : BasePageModel
   {
-    public void OnGet() { }
+    [BindProperty]
+    public List<ToastContainer> Containers { get; set; }
+
+    [BindProperty]
+    public string TopToast { get; set; }
+    [BindProperty]
+    public string BottomToast { get; set; }
+    [BindProperty]
+    public string ToastMessage { get; set; }
+    public void OnGet() {
+      TopToast = "Enter something for top toast";
+      BottomToast = "Enter something for bottom toast";
+
+      ToastMessage = "Simple notification";
+      AddToastError("خطایی رخ داده لطفا بررسی کنید");
+      AddPageError("خطایی جالب رخ داده....");
+      AddAlertSuccess("عملیات درج پیغام با موفقیت انجام شد");
+      AddAlertWarning("شاید عملیات درج پیغام با موفقیت انجام شد");
+      AddAlertError("عملیات درج پیغام با موفقیت انجام نشد");
+    }
   }
 }
--- src/AspnetCoreStarter/wwwroot/js/set-local-token.js
+++ src/AspnetCoreStarter/wwwroot/js/set-local-token.js
@@ -6,35 +6,69 @@ var domain = localStorage.getItem('dabr')
 
 if (checkTokenExit !== null) {
   var tokenjson = JSON.parse(checkTokenExit);
-  console.log(tokenjson.tokenExpireTime)
-  console.log(tokenjson)
+  
+  console.log(tokenjson)  
   console.log(domain)
-
+  var nowDate = new Date().getTime();
   if (domain !== null) {
-    if (Date.parse(tokenjson.tokenExpireTime) < Date.now()) {
+    var tokenExpireDate = convertToDate(tokenjson.tokenExpireTime);
+    console.log(tokenExpireDate)
+    console.log(nowDate)
+    if (tokenExpireDate < nowDate) {
       console.log('token time is Unvalid');
-      if (Date.parse(tokenjson.refreshTokenExpireTime) > Date.now()) {
-        $.ajax({
-          url: domain + "/api/Token/refresh",
-          type: "POST",
-          data: {
-            tokenjson
-          },
-          success: function (response) {
-            if (response.isSuccess === true) {
-              localStorage.removeItem('diam')
-              localStorage.setItem('diam', JSON.stringify(response.data))
-              token_local = response.data.token
-            }
-          },
-          error: function (error) {
-            console.log(error);
+      console.log(tokenjson.refreshTokenExpireTime)
+      var refreshTokenExpireTime = convertToDate(tokenjson.refreshTokenExpireTime);
+      if (refreshTokenExpireTime > nowDate) {
+        console.log("Start Get New Refresh Token")
+        let url = domain + "/api/token/refresh";
+        let data = {
+          token: tokenjson.token,
+          refreshToken: tokenjson.refreshToken
+        };
+        console.log(data)
+        fetch(url, {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json"
           }
-        });
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+            if (data.isSuccess === true) {
+              localStorage.removeItem('diam');
+              localStorage.setItem('diam', JSON.stringify(data.data));
+              console.log(data.data)
+              token_local = data.data.token;
+            }
+          })
+          .catch(error => console.log(error));
+
       }
+      else {
+        console.log('refresh token is expired')
+      }
+    }
+    else {
+      console.log("Token Is Not Expired")
     }
     token_local = tokenjson.token;
   }
 }
+function convertToDate(dateString) {
+  console.log(dateString)
+  var dateParts = dateString.split("/");
+  var timeParts = dateParts[2].split(" ")[1].split(":");
+  var year = parseInt(dateParts[2].split(" ")[0]);
+  var month = parseInt(dateParts[0]) - 1;
+  var day = parseInt(dateParts[1]);
+  var hours = parseInt(timeParts[0]);
+  var minutes = parseInt(timeParts[1]);
+  var seconds = parseInt(timeParts[2]);
 
-console.log(token_local)
+  var utcDate = new Date(Date.UTC(year, month, day, hours, minutes, seconds));
+  return utcDate.getTime();
+}
+
+
